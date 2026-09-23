2f188bb8acfdce76532c0febcf592fc409609a4b
Alexander Ludwig <alexander.ludwig@htlwy.at>
2026-09-23 15:04:28 +0200

    1Version1

---
+++ 001_Schleife/001_Schleife/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Schleife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geben sie eine Zahl ein:");
+            string input = Console.ReadLine();
+            for(int i = 0; input.Length=!i; i++)
+            {
+                Console.WriteLine("Hallo Welt");
+            }
+
+        }
+    }
+}
---
+++ 002_Werkstatt verwaltung/002_Werkzeugausgabe/002_Werkzeugausgabe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Werkzeugausgabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
